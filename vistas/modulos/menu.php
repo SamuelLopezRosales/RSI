b7570d1ca685ff96bef1f2a9a7f954cd91b2e050
Samuel <samuellopezrosales101@gmail.com>
2021-06-04 01:05:28 -0500

    menu_y_footer

---
+++ vistas/modulos/menu.php
@@ -0,0 +1,26 @@
+<aside class="main-sidebar">
+	<section class="sidebar">
+		<ul class="sidebar-menu">
+			<li class="active">
+				<a href="#">
+					<i class="fa fa-home"></i>
+					<span>Inicio</span>
+				</a>
+			</li>
+
+			<li>
+				<a href="#">
+					<i class="fa fa-user"></i>
+					<span>Categorías</span>
+				</a>
+			</li>
+
+			<li>
+				<a href="#">
+					<i class="fa fa-th"></i>
+					<span>Activos Fijos</span>
+				</a>
+			</li>
+		</ul>
+	</section>
+</aside>
